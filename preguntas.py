8566ed97485f141e97dc231bdf5dbf98a2dd2a1a
dsanchezhe <126032152+dsanchezhe@users.noreply.github.com>
2023-03-27 19:41:39 -0500

    Update preguntas.py
    pregunta01

--- preguntas.py
+++ preguntas.py
@@ -11,6 +11,16 @@ Utilice el archivo `data.csv` para resolver las preguntas.
 
 
 """
+import itertools
+from collections import Counter
+from datetime import datetime
+from operator import itemgetter
+
+
+with open ("data.csv", "r") as file:
+        datos = file.readlines()
+datos = [line.replace('\t','|').replace('\n','') for line in datos]
+datos = [line.split('|')for line in datos]
 
 
 def pregunta_01():
@@ -21,8 +31,12 @@ def pregunta_01():
     214
 
     """
-    return
+    
+suma = 0
+    for lista in datos:
+        suma +- int(lista[1])
 
+    return suma
 
 def pregunta_02():
     """
